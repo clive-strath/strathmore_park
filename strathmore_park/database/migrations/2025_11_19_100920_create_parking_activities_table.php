886b5775df45a883e408872ced75589929505dbe
Ced00-26 <cedricmichael.kagumba@strathmore.edu>
2025-11-23 19:58:02 +0300

    adding migrations for users,lots,activities and seeders

--- strathmore_park/database/migrations/2025_11_19_100920_create_parking_activities_table.php
+++ strathmore_park/database/migrations/2025_11_19_100920_create_parking_activities_table.php
@@ -6,20 +6,19 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('parking_activities', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('parking_lot_id')->constrained()->onDelete('cascade');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->enum('action', ['entry', 'exit']);
+            $table->integer('spots_before');
+            $table->integer('spots_after');
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('parking_activities');
--- strathmore_park/database/seeders/ParkingLotSeeder.php
+++ strathmore_park/database/seeders/ParkingLotSeeder.php
@@ -2,16 +2,21 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\ParkingLot;
 
 class ParkingLotSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        //
+        $lots = [
+            ['name' => 'A', 'total_spots' => 50, 'available_spots' => 50, 'is_active' => true],
+            ['name' => 'B', 'total_spots' => 40, 'available_spots' => 40, 'is_active' => true],
+            ['name' => 'C', 'total_spots' => 30, 'available_spots' => 30, 'is_active' => true],
+        ];
+
+        foreach ($lots as $lot) {
+            ParkingLot::create($lot);
+        }
     }
 }
